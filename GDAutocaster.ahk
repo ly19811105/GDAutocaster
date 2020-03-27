ly19811105/GDAutocaster

commit 8f268b75f104b793e6b90b81d997e88154478978

change autocasting toggle key to not work with modifiers such as Alt

GDAutocaster.ahk
@@ -50,7 +50,7 @@ for not_used, key in skill_key_list
     if Configured(toggle_key)
     {
         areTimersToggled[key] := false
-        AddHotkey("*" . toggle_key, Func("ToggleTimer").Bind(key))
+        AddHotkey(toggle_key, Func("ToggleTimer").Bind(key))
         timers_to_toggle.Push(key)
     }    
     

commit 38e731b8f023c7bd1c1999ee9555846e4243a5c1

fix bug caused by 8f268b75f104b793e6b90b81d997e88154478978

GDAutocaster.ahk
@@ -50,7 +50,7 @@ for not_used, key in skill_key_list
     if Configured(toggle_key)
     {
         areTimersToggled[key] := false
-        hotkeys_collector.AddHotkey(toggle_key, Func("ToggleTimer").Bind(key))
+        hotkeys_collector.AddHotkey("$" . toggle_key, Func("ToggleTimer").Bind(key))
         timers_to_toggle.Push(key)
     }    
     